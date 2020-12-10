776e2d87d147bd53082dae2c045ec95fa5c6d099
Taha Abdullah <taha.abdullah20@gmail.com>
2020-12-10 11:48:17 +0300

    Added RandomWords and RandomWordsState classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:english_words/english_words.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return
+    final wordPair = WordPair.random();
+
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.purple[900]),
+      home: RandomWords()); 
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('WordPair Generator')),
+      body: _buildList(), 
+    );
   }
 }
