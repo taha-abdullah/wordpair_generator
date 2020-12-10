ebfee1083fb3d91f5cd5b55fe350914ad084acee
Taha Abdullah <taha.abdullah20@gmail.com>
2020-12-10 12:58:25 +0300

    Added a seperate page for saved words, along with navigation

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.purple[900]),
       home: RandomWords()); 
   }
 }
--- lib/random_words.dart
+++ lib/random_words.dart
@@ -9,7 +9,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _randomWordPairs = <WordPair>[];
-  final _savedWordPairs = Set<WordPair>();
+  final _savedWordPairs = Set<WordPair>(); 
 
   Widget _buildList() {
     return ListView.builder(
@@ -30,16 +30,83 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+
     return ListTile(
-      title: Text(pair.asPascalCase, 
-      style: TextStyle(fontSize: 18.0)
-    ));
+      title: Text(
+        pair.asPascalCase, 
+        style: TextStyle(fontSize: 18.0)
+      ),
+
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border, 
+        color: alreadySaved ? Colors.red : null
+      ),
+
+      onTap: () {
+        setState(() {
+          if(alreadySaved) {
+            _savedWordPairs.remove(pair);
+          }
+          else {
+            _savedWordPairs.add(pair);
+          }
+        });
+      },
+
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+           final Iterable<ListTile> tiles = 
+           _savedWordPairs.map((WordPair pair) {
+             return ListTile(
+               title: Text(pair.asPascalCase,
+                style: TextStyle(fontSize: 16.0)),
+            );
+           });
+
+           final List<Widget> divided = 
+            ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList();
+
+            return Scaffold(
+
+              appBar: AppBar(
+                title: Text('Saved WordPairs'),
+              ),
+
+              body: ListView(
+                children: divided
+              ),
+
+            );
+        } 
+      )
+    );
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('WordPair Generator')),
+
+      appBar: AppBar(
+        title: Text('WordPair Generator'), 
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
+      ),
+
       body: _buildList(), 
+
     );
-  }
-}
+  } //build
+
+} //RandomWordsState
